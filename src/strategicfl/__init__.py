d24b9710c4683c2ea14e5ccf3993f87e689ea2f2
dimitarch <dimitar_ch@hotmail.com>
2025-08-20 17:34:26 -0500

    minor fixes

--- src/strategicfl/__init__.py
+++ src/strategicfl/__init__.py
@@ -1,1 +1,17 @@
+"""
+StrategicFL: A Framework for Strategic Federated Learning
+
+A Python framework for simulating strategic federated learning scenarios where
+clients can behave adversarially by manipulating gradient updates.
+"""
+
 __version__ = "0.1.0"
+__author__ = "Dimitar Chakarov"
+
+# Core imports for easy access
+from .agents import Client, Server
+
+__all__ = [
+    "Client",
+    "Server",
+]
