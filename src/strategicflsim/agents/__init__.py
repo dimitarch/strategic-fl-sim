6c7a44ca1f6c4a9c40c5024d2a9d872e565d8fcb
dimitarch <dimitar_ch@hotmail.com>
2025-10-20 15:11:30 -0500

    fix init distributed

--- src/strategicflsim/agents/__init__.py
+++ src/strategicflsim/agents/__init__.py
@@ -1,6 +1,7 @@
 from .base_client import BaseClient
 from .base_server import BaseServer
 from .client import Client
+from .distributed import DistributedClient, DistributedServer
 from .server import Server
 
 __all__ = [
