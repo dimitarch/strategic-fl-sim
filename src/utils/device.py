b798ff644b4dda7e82e9cb663aa8a443af90e57a
dimitarch <dimitar_ch@hotmail.com>
2025-10-19 20:46:50 -0500

    cleanup formatting

--- src/utils/device.py
+++ src/utils/device.py
@@ -3,10 +3,8 @@ import torch
 
 def get_device() -> torch.device:
     if torch.cuda.is_available():
-        device = torch.device("cuda")  # GPU
+        return torch.device("cuda")  # GPU
     elif torch.backends.mps.is_built():
-        device = torch.device("mps")  # Apple M-series
+        return torch.device("mps")  # Apple M-series
     else:
-        device = torch.device("cpu")  # CPU
-
-    return device
+        return torch.device("cpu")  # CPU
