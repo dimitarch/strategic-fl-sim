970149227e648c1d537b7dab62d67116d1aff4c7
dimitarch <dimitar_ch@hotmail.com>
2025-10-20 23:52:54 -0500

    femnist dataloader initialize tensors then fill

--- experiments/femnist/femnistdataloader.py
+++ experiments/femnist/femnistdataloader.py
@@ -12,25 +12,52 @@ class FEMNISTDataset(Dataset):
         else:
             self.client_names = client_names
 
-        # Collect all data from specified clients
-        self.inputs = []
-        self.labels = []
+        total_samples = sum(
+            len(data_dict[name]["x"]) for name in self.client_names if name in data_dict
+        )
 
-        for client_name in self.client_names:
-            if client_name in data_dict:
-                client_inputs = torch.tensor(data_dict[client_name]["x"]).float()
-                client_inputs = client_inputs.reshape(-1, 1, 28, 28)
-                client_labels = torch.tensor(data_dict[client_name]["y"]).long()
-
-                self.inputs.append(client_inputs)
-                self.labels.append(client_labels)
-
-        if self.inputs:
-            self.inputs = torch.cat(self.inputs, dim=0)
-            self.labels = torch.cat(self.labels, dim=0)
-        else:
+        if total_samples == 0:
             self.inputs = torch.empty(0, 1, 28, 28)
             self.labels = torch.empty(0, dtype=torch.long)
+            return
+
+        self.inputs = torch.empty((total_samples, 1, 28, 28), dtype=torch.float32)
+        self.labels = torch.empty(total_samples, dtype=torch.long)
+
+        # Fill in-place
+        offset = 0
+        for client_name in self.client_names:
+            if client_name in data_dict:
+                n = len(data_dict[client_name]["x"])
+                self.inputs[offset : offset + n] = (
+                    torch.tensor(data_dict[client_name]["x"])
+                    .view(-1, 1, 28, 28)
+                    .float()
+                )
+                self.labels[offset : offset + n] = torch.tensor(
+                    data_dict[client_name]["y"]
+                ).long()
+                offset += n
+
+        # # Collect all data from specified clients
+        # self.inputs = []
+        # self.labels = []
+
+        # for client_name in self.client_names:
+        #     if client_name in data_dict:
+        #         client_inputs = torch.tensor(data_dict[client_name]["x"]).float()
+        #         client_inputs = client_inputs.reshape(-1, 1, 28, 28)
+        #         client_labels = torch.tensor(data_dict[client_name]["y"]).long()
+
+        #         self.inputs.append(client_inputs)
+        #         self.labels.append(client_labels)
+
+        # if self.inputs:
+        #     self.inputs = torch.cat(self.inputs, dim=0)
+        #     self.labels = torch.cat(self.labels, dim=0)
+        # else:
+        #     self.inputs = torch.empty(0, 1, 28, 28)
+        #     self.labels = torch.empty(0, dtype=torch.long)
 
     def __len__(self):
         return len(self.inputs)
